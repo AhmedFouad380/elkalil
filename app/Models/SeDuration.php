08d74f9892713553a05a610b042ee2c30298e6b4
ahmed fouad <asd09505@gmail.com>
2021-12-15 16:28:42 +0200

    a

---
+++ app/Models/SeDuration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SeDuration extends Model
+{
+    use HasFactory;
+
+    protected  $table = 'se_duration';
+
+}
---
+++ app/Models/SeKnowUs.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SeKnowUs extends Model
+{
+    use HasFactory;
+
+    protected  $table = 'se_know_us';
+
+}
