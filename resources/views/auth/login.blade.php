1446657a424ae8562ed7150f24bc3cf4e2b2dfd8
TarekMandour <81233440+TarekMandour@users.noreply.github.com>
2021-12-18 12:47:16 +0200

    login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,33 +8,30 @@
 
 @section('content')
 <!--begin::Authentication - Sign-in -->
-<div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url({{asset('admin/assets/media/illustrations/sketchy-1/14.png')}}">
+<div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-cover" style="background-image: url({{asset('admin/assets/media/illustrations/sketchy-1/14-2.png')}}">
 
     <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
-        <!--begin::Logo-->
-        <a href="../../demo16/dist/index.html" class="mb-12">
-            <img alt="Logo" src="{{asset('admin/assets/media/logos/logo-1.svg')}}" class="h-40px" />
-        </a>
-        <!--end::Logo-->
+        
         <!--begin::Wrapper-->
         <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
             <!--begin::Form-->
             <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="#">
                 <!--begin::Heading-->
                 <div class="text-center mb-10">
+                    <!--begin::Logo-->
+                    <a href="../../demo16/dist/index.html" class="mb-12">
+                        <img alt="Logo" src="{{asset('admin/assets/media/logos/logo-dark.png')}}" class="h-150px" />
+                    </a>
+                    <!--end::Logo-->
                     <!--begin::Title-->
-                    <h1 class="text-dark mb-3">Sign In to Metronic</h1>
+                    <h1 class="text-danger mt-3">تسجيل الدخول</h1>
                     <!--end::Title-->
-                    <!--begin::Link-->
-                    <div class="text-gray-400 fw-bold fs-4">New Here?
-                    <a href="../../demo16/dist/authentication/layouts/basic/sign-up.html" class="link-primary fw-bolder">Create an Account</a></div>
-                    <!--end::Link-->
                 </div>
                 <!--begin::Heading-->
                 <!--begin::Input group-->
                 <div class="fv-row mb-10">
                     <!--begin::Label-->
-                    <label class="form-label fs-6 fw-bolder text-dark">Email</label>
+                    <label class="form-label fs-6 fw-bolder text-dark">اسم المستخدم</label>
                     <!--end::Label-->
                     <!--begin::Input-->
                     <input class="form-control form-control-lg form-control-solid" type="text" name="email" autocomplete="off" />
@@ -46,10 +43,10 @@
                     <!--begin::Wrapper-->
                     <div class="d-flex flex-stack mb-2">
                         <!--begin::Label-->
-                        <label class="form-label fw-bolder text-dark fs-6 mb-0">Password</label>
+                        <label class="form-label fw-bolder text-dark fs-6 mb-0">كلمة المرور</label>
                         <!--end::Label-->
                         <!--begin::Link-->
-                        <a href="../../demo16/dist/authentication/layouts/basic/password-reset.html" class="link-primary fs-6 fw-bolder">Forgot Password ?</a>
+                        <a href="../../demo16/dist/authentication/layouts/basic/password-reset.html" class="link-danger fs-6 fw-bolder">نسيت كلمة المرور ؟</a>
                         <!--end::Link-->
                     </div>
                     <!--end::Wrapper-->
@@ -61,27 +58,12 @@
                 <!--begin::Actions-->
                 <div class="text-center">
                     <!--begin::Submit button-->
-                    <button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
-                        <span class="indicator-label">Continue</span>
-                        <span class="indicator-progress">Please wait...
+                    <button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-danger w-100 mb-5">
+                        <span class="indicator-label">دخول</span>
+                        <span class="indicator-progress">انتظر لحظات...
                         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                     </button>
                     <!--end::Submit button-->
-                    <!--begin::Separator-->
-                    <div class="text-center text-muted text-uppercase fw-bolder mb-5">or</div>
-                    <!--end::Separator-->
-                    <!--begin::Google link-->
-                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-                    <img alt="Logo" src="{{asset('admin/assets/media/svg/brand-logos/google-icon.svg')}}" class="h-20px me-3" />Continue with Google</a>
-                    <!--end::Google link-->
-                    <!--begin::Google link-->
-                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-                    <img alt="Logo" src="{{asset('admin/assets/media/svg/brand-logos/facebook-4.svg')}}" class="h-20px me-3" />Continue with Facebook</a>
-                    <!--end::Google link-->
-                    <!--begin::Google link-->
-                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
-                    <img alt="Logo" src="{{asset('admin/assets/media/svg/brand-logos/apple-black.svg')}}" class="h-20px me-3" />Continue with Apple</a>
-                    <!--end::Google link-->
                 </div>
                 <!--end::Actions-->
             </form>
