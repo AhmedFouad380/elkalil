bf5838d305721d9eb0f61b4a82f0fd9f950b31db
ahmed fouad <asd09505@gmail.com>
2021-12-21 00:30:55 +0200

    a

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Auth;
 class AuthController extends Controller
 {
     public function Login(Request $request){
 
         if($user = User::where('phone',$request->phone)->where('password',sha1($request->password))->first()){
             Auth::login($user);
-            return redirect('/AdminPanel');
+            return redirect('/');
         }else{
             return back()->with('message','Failed');
         }
